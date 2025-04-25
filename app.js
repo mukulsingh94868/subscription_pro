7c13d0e4f589e73d8c10ce6f13870a218b926717
mukul65 <mukul.singh@zentekinfosoft.com>
2025-04-25 15:19:42 +0530

    add mongoose file

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import { PORT } from "./config/env.js";
 import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
+import connectToDatabase from "./database/mongodb.js";
 
 const app = express();
 
@@ -16,8 +17,9 @@ app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
     console.log(`Subscription Tracking app is running on http://localhost:${PORT}`);
+    await connectToDatabase();
 });
 
 export default app;
---
+++ database/mongodb.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import { DB_URI, NODE_ENV } from "../config/env.js";
+
+if (!DB_URI) {
+    throw new Error("DB_URI is not defined");
+}
+
+const connectToDatabase = async () => {
+    try {
+        await mongoose.connect(DB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log(`Connected to MongoDB in ${NODE_ENV} mode`);
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+        // process.exit(1);
+    }
+};
+
+export default connectToDatabase;
