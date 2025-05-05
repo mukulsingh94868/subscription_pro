1fcfd1c682f3fed06b05d0685d8b2d8aa6a4a2d4
mukulsingh94868 <mukulsingh94868@gmail.com>
2025-05-05 18:47:18 +0530

    arcjet

---
+++ middleware/arcjet.middleware.js
@@ -0,0 +1,25 @@
+import aj from "../config/arcjet.js";
+
+const arcjetMiddleware = async (req, res, next) => {
+  try {
+    const decision = await aj.protect(req, { requested: 1 });
+    console.log('decision', decision);
+    if (decision.isDenied()) {
+      if (decision.reason.isRateLimit()) {
+        return res.status(429).json({ error: "Rate limit exceeded" });
+      }
+      if (decision.reason.isBot()) {
+        return res.status(403).json({ error: "Bot detected" });
+      }
+
+      return res.status(403).json({ error: "Access Denied" });
+    }
+
+    next();
+  } catch (error) {
+    console.log("error", error);
+    next(error);
+  }
+};
+
+export default arcjetMiddleware;
