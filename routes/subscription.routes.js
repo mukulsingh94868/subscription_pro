ef06fa55babbc29364cc77f09ce731aa7b04251f
mukulsingh94868 <mukulsingh94868@gmail.com>
2025-05-05 19:09:45 +0530

    subscription api

--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -1,37 +1,38 @@
 import { Router } from "express";
+import {
+  createSubscription,
+  getUserSubscriptions,
+} from "../controller/subscription.controller.js";
+import authorize from "../middleware/auth.middleware.js";
 
 const subscriptionRouter = Router();
 
-subscriptionRouter.get('/', (req, res) => {
-    res.send({ title: "Get all subscriptions" });
+subscriptionRouter.get("/", (req, res) => {
+  res.send({ title: "Get all subscriptions" });
 });
 
-subscriptionRouter.get('/:id', (req, res) => {
-    res.send({ title: "Get subscriptions details" });
+subscriptionRouter.get("/:id", (req, res) => {
+  res.send({ title: "Get subscriptions details" });
 });
 
-subscriptionRouter.post('/', (req, res) => {
-    res.send({ title: "Create subscriptions" });
+subscriptionRouter.post("/", authorize, createSubscription);
+
+subscriptionRouter.put("/:id", (req, res) => {
+  res.send({ title: "Update subscriptions" });
 });
 
-subscriptionRouter.put('/:id', (req, res) => {
-    res.send({ title: "Update subscriptions" });
+subscriptionRouter.delete("/:id", (req, res) => {
+  res.send({ title: "Delete all subscriptions" });
 });
 
-subscriptionRouter.delete('/:id', (req, res) => {
-    res.send({ title: "Delete all subscriptions" });
+subscriptionRouter.get("/user/:id", authorize, getUserSubscriptions);
+
+subscriptionRouter.put("/:id/cancel", (req, res) => {
+  res.send({ title: "Cancel subscriptions" });
 });
 
-subscriptionRouter.get('/user/:id', (req, res) => {
-    res.send({ title: "Get all users subscriptions" });
-});
-
-subscriptionRouter.put('/:id/cancel', (req, res) => {
-    res.send({ title: "Cancel subscriptions" });
-});
-
-subscriptionRouter.get('/upcoming-renewals', (req, res) => {
-    res.send({ title: "GET upcoming renewals" });
+subscriptionRouter.get("/upcoming-renewals", (req, res) => {
+  res.send({ title: "GET upcoming renewals" });
 });
 
 export default subscriptionRouter;
