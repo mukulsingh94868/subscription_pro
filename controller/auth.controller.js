ac79f87c233841252f0efe70e568b90dc1f76799
mukulsingh94868 <mukulsingh94868@gmail.com>
2025-05-05 13:50:04 +0530

    auth and user controller

---
+++ controller/auth.controller.js
@@ -0,0 +1,94 @@
+import mongoose from "mongoose";
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import { JWT_SECRET, JWT_EXPIRES_IN } from "../config/env.js";
+import jwt from "jsonwebtoken";
+
+export const signUp = async (req, res, next) => {
+  const session = await mongoose.startSession();
+  session.startTransaction();
+
+  try {
+    const { username, email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+
+    // check if a user already exit
+    if (existingUser) {
+      const error = new Error("User already exists");
+      error.statusCode = 409;
+      throw error;
+    }
+
+    // Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUsers = await User.create([
+      {
+        username,
+        email,
+        password: hashedPassword,
+      },
+    ]);
+
+    const token = jwt.sign({ userId: newUsers[0]._id }, JWT_SECRET, {
+      expiresIn: JWT_EXPIRES_IN,
+    });
+
+    await session.commitTransaction();
+    session.endSession();
+
+    res.status(201).json({
+      success: true,
+      message: "User created successfully",
+      data: {
+        token,
+        user: newUsers[0],
+      },
+    });
+  } catch (error) {
+    await session.abortTransaction();
+    session.endSession();
+    next(error);
+  }
+};
+
+export const signIn = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      const error = new Error("User not found");
+      error.statusCode = 404;
+      throw error;
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      const error = new Error("Invalid credentials");
+      error.statusCode = 401;
+      throw error;
+    }
+
+    const token = jwt.sign({ userId: user._id }, JWT_SECRET, {
+      expiresIn: JWT_EXPIRES_IN,
+    });
+
+    res.status(200).json({
+        success: true,
+        message: "User signed in successfully",
+        data: {
+          token,
+          user,
+        },
+    })
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signOut = async (req, res) => {
+  res.send({ title: "Sign Out" });
+};
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,25 +1,23 @@
 import { Router } from "express";
+import { getUser, getUsers } from "../controller/user.controller.js";
+import authorize from "../middleware/auth.middleware.js";
 
 const userRouter = Router();
 
-userRouter.get('/', (req, res) => {
-    res.send({ title: "Get all users" });
+userRouter.get("/", authorize, getUsers);
+
+userRouter.get("/:id", getUser);
+
+userRouter.post("/", (req, res) => {
+  res.send({ title: "CREATE USER" });
 });
 
-userRouter.get('/:id', (req, res) => {
-    res.send({ title: "Get user details" });
+userRouter.put("/:id", (req, res) => {
+  res.send({ title: "UPDATE USER" });
 });
 
-userRouter.post('/', (req, res) => {
-    res.send({ title: "CREATE USER" });
-});
-
-userRouter.put('/:id', (req, res) => {
-    res.send({ title: "UPDATE USER" });
-});
-
-userRouter.delete('/:id', (req, res) => {
-    res.send({ title: "DELETE USER" });
+userRouter.delete("/:id", (req, res) => {
+  res.send({ title: "DELETE USER" });
 });
 
 export default userRouter;
